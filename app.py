f8468633427988f55c3726e2479d787e11922b66
Mihaela Ilieva <mih1711@abv.bg>
2025-05-06 19:29:00 +0300

    Added database connection with models

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
-
+from db import db
+from models.user import User
+from models.room import Room
+from models.reservation import Reservation
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reservation_system.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
 api = Api(app)
+
+with app.app_context():
+    db.create_all()
 
 #add logic here
 
