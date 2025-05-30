a056e665064d55d90341099e11e7a49c1c89084d
Mihaela Ilieva <mih1711@abv.bg>
2025-05-30 16:52:08 +0300

    Added register window

--- reservation_system/main_page.py
+++ reservation_system/main_page.py
@@ -46,7 +46,7 @@ class StartWindow:
     def open_register(self):
         self.master.withdraw()
         register_window = tk.Toplevel()
-        #RegisterWindow(register_window)
+        RegisterWindow(register_window)
 
 class LoginWindow:
     def __init__(self, master):
@@ -101,7 +101,66 @@ class LoginWindow:
                 messagebox.showerror("Грешка", "Невалидни данни за вход")
         except Exception as e:
             messagebox.showerror("Грешка", f"Проблем с връзката към сървъра:\n{e}")
-            
+
+class RegisterWindow:
+    def __init__(self, master):
+        self.master = master
+        master.title("Вход")
+        master.geometry("1720x1060")
+        master.config(bg=MAIN_COLOUR)
+        
+        self.title_label = Label(master, text="LuxeHalls - правилното място за избор на зала за вашето събитие", font=("Tahoma", 26), fg=SECONDARY_COLOUR, background=MAIN_COLOUR).pack(pady=35)
+        
+        image_path = os.path.join(os.path.dirname(__file__), "images", "image.png")
+        pil_image = Image.open(image_path)
+
+       
+        max_width, max_height = 650, 350
+        pil_image.thumbnail((max_width, max_height))
+
+        self.background_image = ImageTk.PhotoImage(pil_image)
+
+        image_label = Label(self.master, image=self.background_image, borderwidth=0)
+        image_label.pack(pady=35)
+        
+        self.email_label = Label(master, text="Имейл:",font=("Tahoma", 14), fg=SECONDARY_COLOUR , background=MAIN_COLOUR).pack(pady=10)
+        self.email_entry = Entry(master, width=20)
+        self.email_entry.pack(pady=10)
+
+        self.username_label = Label(master, text="Потребителско име:",font=("Tahoma", 14), fg=SECONDARY_COLOUR , background=MAIN_COLOUR).pack(pady=10)
+        self.username_entry = Entry(master, width=20)
+        self.username_entry.pack(pady=10)
+        
+        self.password_label = Label(master, text="Парола:", font=("Tahoma", 14), fg=SECONDARY_COLOUR, background=MAIN_COLOUR).pack(pady=10)
+        self.password_entry = Entry(master, width=20, show="*")
+        self.password_entry.pack(pady=10)
+
+        self.register_button = Button(master, text="Регистрация", command=self.register, width=20, font=("Tahoma", 16), background=SECONDARY_COLOUR).pack(pady=20)
+
+
+    def register(self):
+        email = self.email_entry.get()
+        name = self.username_entry.get()
+        password = self.password_entry.get()
+
+        if not (email and name and password):
+            messagebox.showerror("Грешка", "Всички полета са задължителни!")
+            return
+
+        try:
+            response = requests.post(f"{USER_API}/register", json={
+                "username": name,
+                "password": password,
+                "email": email,
+            })
+            if response.status_code == 201:
+                messagebox.showinfo("Успех", "Регистрацията е успешна.")
+                self.master.destroy()
+                #UserWindow(name, None)
+            else:
+                messagebox.showerror("Грешка", f"Неуспешна регистрация: {response.text}")
+        except Exception as e:
+            messagebox.showerror("Грешка", f"Проблем с връзката към сървъра:\n{e}")
 
 if __name__ == "__main__":
     root = tk.Tk()
